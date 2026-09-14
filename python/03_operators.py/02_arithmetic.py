86aedbf8ef7bf2f174c44183ccf19fd29feb1f42
Grishumaniya2104 <maniyatesh@gmail.com>
2026-09-14 15:44:33 +0530

    Add files via upload

---
+++ python/03_operators.py/02_arithmetic.py
@@ -0,0 +1,42 @@
+#arithmetic operator
+
+#addition +
+
+num1=10;
+
+mum2=20;
+
+result= num1 + num2;
+print("addition,result")
+
+#subtration -
+
+result= num2 - num1;
+print("subtration, result")
+
+
+#multiplication *
+
+result= num1 * num2;
+print("multiplication, result")
+
+
+#division /
+
+result= num2/num1;
+print("division, result")
+
+
+#floor division //
+
+result= num2//num1;
+print("floor division, result")
+
+#modulo %
+result= num2 % num1;
+print("modulo, result")
+
+#exponential **
+
+result= num2**3;
+print("exponential, result")
